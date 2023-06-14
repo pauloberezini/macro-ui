9323a724ae1ede204b77d46fac6f3888a9be44f1
pauloberezini <paulo.berezini@gmail.com>
2023-06-14 19:12:28 +0300

    date format changed

--- src/app/model/date-format-pipe.ts
+++ src/app/model/date-format-pipe.ts
@@ -4,6 +4,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class DateFormatPipe implements PipeTransform {
   transform(value: string): string {
     const date = new Date(value);
-    return date.toLocaleDateString();
+    date.toLocaleDateString();
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-based
+    const year = date.getFullYear();
+    return `${day}-${month}-${year}`;
   }
 }
