f0e76ce3d1685a106c6a34b0b45699145f6efc1c
Pavel Bereznichenko <paulo.berezini@gmail.com>
2024-02-24 11:19:06 +0200

    updated date format pipe

--- src/app/model/date-format-pipe.ts
+++ src/app/model/date-format-pipe.ts
@@ -4,10 +4,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class DateFormatPipe implements PipeTransform {
   transform(value: string): string {
     const date = new Date(value);
-    date.toLocaleDateString();
-    const day = String(date.getDate()).padStart(2, '0');
-    const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-based
-    const year = date.getFullYear();
+    // Use getUTC* methods to ensure we're working with UTC dates
+    const day = String(date.getUTCDate()).padStart(2, '0');
+    const month = String(date.getUTCMonth() + 1).padStart(2, '0'); // Months are zero-based
+    const year = date.getUTCFullYear();
     return `${day}-${month}-${year}`;
   }
 }
