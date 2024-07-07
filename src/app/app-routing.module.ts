81a97e98d1642ebac2ab8b4fb093eaa087a48e5a
Pavel Bereznichenko <paulo.berezini@gmail.com>
2024-07-07 19:38:55 +0300

    maintenance removed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {LoginComponent} from "./login/login.component";
-import {MaintenanceComponent} from "./maintenance/maintenance.component";
 
 const routes: Routes = [
-  { path: 'maintenance', component: MaintenanceComponent },
   { path: 'login', component: LoginComponent },
-  { path: '**', redirectTo: 'maintenance' }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -55,7 +55,6 @@ import {PieChartComponent} from "./pie-chart/pie-chart.component";
 import {DataMessageComponent} from "./util/data-message/data-message.component";
 import {GasComponent} from "./dynamic-component/gas/gas.component";
 import {LoginComponent} from "./login/login.component";
-import {ErrorInterceptorService} from "./interceptors/error.service";
 
 
 const routes: Routes = [
@@ -143,7 +142,6 @@ const routes: Routes = [
     RouterModule
   ],
   providers: [
-    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptorService, multi: true},
     DateFormatPipe,
     {
       provide: HTTP_INTERCEPTORS,
