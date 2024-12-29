6873104c244cd7576c3d2b0affddf52890942971
Lev Berlinkov <lev.b@greeninvoice.co.il>
2024-12-29 17:58:07 +0200

    update columns

--- src/app/insiders-page/insiders/insiders.component.ts
+++ src/app/insiders-page/insiders/insiders.component.ts
@@ -1,6 +1,6 @@
 import {AfterViewInit, Component, ViewChild} from '@angular/core';
-import {StockDataService} from "../services/stock-data.service";
-import {InsiderData} from '../model/InsiderData';
+import {StockDataService} from "../../services/stock-data.service";
+import {InsiderData} from '../../model/InsiderData';
 import {MatSort} from "@angular/material/sort";
 import {MatTableDataSource} from "@angular/material/table";
 
