3bf8c53a5a6d430a037a24c287e2b5ee64c658ad
Pavel Bereznichenko <paulo.berezini@gmail.com>
2025-02-28 12:55:12 +0200

    updated home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,4 +15,24 @@ import {MatCardModule} from "@angular/material/card";
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  testimonials = [
+    {
+      text: 'The most comprehensive macroeconomic analysis platform we\'ve used...',
+      name: 'Sarah Johnson',
+      position: 'Chief Economist, Global Bank',
+      photo: 'assets/team/sarah.jpg'
+    },
+    {
+      text: 'Revolutionized our research capabilities overnight',
+      name: 'Michael Chen',
+      position: 'Portfolio Manager, Hedge Fund',
+      photo: 'assets/team/michael.jpg'
+    }
+  ];
+
+  currentStats = {
+    countries: 57,
+    indicators: 2340,
+    updatesDaily: 1200000
+  };
 }
