e2af1f138e0b4b648504ec02617e42ffd304bb64
Pavel Bereznichenko <paulo.berezini@gmail.com>
2025-06-03 18:27:11 +0300

    updated inputs for custom stocks

--- src/app/chart-year-component/chart-year-component.component.ts
+++ src/app/chart-year-component/chart-year-component.component.ts
@@ -11,6 +11,7 @@ import {MatToolbarModule} from "@angular/material/toolbar";
 import {StockData} from "../model/stock-data";
 import {MatInput} from "@angular/material/input";
 
+
 @Component({
   selector: 'app-chart-year-component',
   templateUrl: './chart-year-component.component.html',
@@ -83,19 +84,17 @@ export class ChartYearComponentComponent implements OnInit {
     this.resizeEvent.pipe(
       debounceTime(300) // Wait for 300ms pause in events to avoid thrashing
     ).subscribe(() => {
-      this.createChart();
+      this.createSeasonalChart(null);
     });
   }
 
   ngOnInit(): void {
     this.canvasId = 'chart-year-component-' + Math.random().toString(36).substring(2, 15);
     this.canvasIdSeasonal = 'chart-year-component-seasonal-' + Math.random().toString(36).substring(2, 15);
-    this.createChart();
     this.createSeasonalChart('dropdown');
   }
 
   async getData(source: string) {
-    this.createChart();
     this.createSeasonalChart(source);
     this.valueChanged.emit(this.stockName);
   }
@@ -127,13 +126,8 @@ export class ChartYearComponentComponent implements OnInit {
   }
 
   createSeasonalChart(source: string) {
-    let marketstackSymbol: string;
-    if (source === 'dropdown') {
-      this.ticker = this.getMarketstackSymbol(this.stockName);
-      marketstackSymbol = this.getMarketstackSymbol(this.stockName);
-    } else if (source === 'input') {
-      marketstackSymbol = this.ticker
-    }
+    let marketstackSymbol: string = this.getTicker(source);
+
     if (!marketstackSymbol) {
       return;
     }
@@ -225,147 +219,14 @@ export class ChartYearComponentComponent implements OnInit {
     });
   }
 
-
-  createChart() {
-    this.stockDataService.getStockAlphaData25(this.stockName, this.election).subscribe((response: any) => {
-      let responseValues: any [] = [];
-      response.data.forEach((item: any) => {
-        responseValues.push(item.value);
-
-      });
-      this.values = responseValues;
-      const dailyLabels = this.generateDailyLabels(2023)
-
-      // this.stockDataService.getStockYearAllDaily('GBPUSD').subscribe((response: any) => {
-      if (this.chart) {
-        this.chart.destroy();
-      }
-      Chart.register(annotationPlugin);
-
-      let years: string = this.stockName + ' ' + response.data[0].rangeValue + ' years';
-      if (this.election != 'regular') {
-        years = this.stockName + ' ' + 6 + ' years';
-      }
-
-      this.chart = new Chart(this.canvasId, {
-        type: 'line',
-        data: {
-          labels: dailyLabels,
-          datasets: [
-            {
-              label: years,
-              data: responseValues,
-              borderColor: 'rgb(255, 99, 132)',
-              backgroundColor: 'rgba(255, 99, 132, 0.2)',
-              borderWidth: 0.1,
-              pointBackgroundColor: 'rgb(255, 99, 132)',
-              tension: 0.4,
-              pointStyle: false,
-              fill: 'start'
-            }
-            // ,          {
-            //   label: "Cloud Coverage(%) in Lahore",
-            //   data: cloudData,
-            //   borderColor: 'rgb(54, 162, 235)',
-            //   backgroundColor: 'rgba(54, 162, 235, 0.2)',
-            //   borderWidth: 2,
-            //   pointBackgroundColor: 'rgb(54, 162, 235)',
-            // }
-          ]
-        },
-        options: {
-          aspectRatio: 2.5,
-
-          animations: {
-            radius: {
-              duration: 0.1,
-              easing: 'linear',
-              loop: (context) => context.active
-            }
-          },
-
-          interaction: {
-            intersect: false,
-          },
-          elements: {
-            line: {
-              tension: 0.5
-            },
-          },
-
-          scales: {
-            y: {
-              grid: {
-                color: 'rgba(0, 0, 0, 0.1)',
-              }
-            },
-            x: {
-              type: 'time',
-              time: {
-                unit: 'month',
-                displayFormats: {
-                  month: 'MMM'
-                }
-              },
-              grid: {
-                color: 'rgba(0, 0, 0, 0.1)',
-              },
-              ticks: {
-                autoSkip: true,
-                maxTicksLimit: 12
-              }
-            }
-          },
-          plugins: {
-            annotation: {
-              // annotations: {
-              //   watermark: {
-              //     type: 'label',
-              //     position: 'center',
-              //     color: 'rgba(0, 0, 0, 0.2)',
-              //     opacity: 0.05,
-              //     // backgroundColor: 'rgba(0, 0, 0, 0.05)', // low opacity
-              //     content: 'macro.berezini.com',
-              //     font: {
-              //       size: 100,
-              //       style: 'normal',
-              //       family: 'Fantasy'
-              //     },
-              //     textAlign: 'center'
-              //   }
-              // }
-            },
-            tooltip: {
-              callbacks: {
-                title: (tooltipItems) => {
-                  // Usually only one item per tooltip in a line chart,
-                  // so we just grab the first item
-                  if (!tooltipItems.length) return '';
-
-                  const xValue = tooltipItems[0].parsed.x;  // The raw x-value (timestamp, etc.)
-                  const dateObj = new Date(xValue);
-
-                  // Format date to month & day only, e.g., “Aug 13”
-                  // Using built-in toLocaleDateString for quickness:
-                  return dateObj.toLocaleDateString('en-US', {month: 'short', day: 'numeric'});
-                },
-                label: context => {
-                  const dateObject = new Date(context.parsed.x);
-                  const averageData = context.parsed.y;
-                  return `Average point: ${averageData}`;
-                }
-              }
-            },
-            legend: {
-              labels: {
-                font: {
-                  size: 14,
-                }
-              }
-            }
-          }
-        }
-      });
-    });
+  getTicker(source: string) {
+    if (source === 'dropdown') {
+      this.ticker = this.getMarketstackSymbol(this.stockName);
+    }
+    if (this.ticker && this.ticker.length > 0) {
+      return this.ticker;
+    } else {
+      return this.getMarketstackSymbol(this.stockName);
+    }
   }
 }
